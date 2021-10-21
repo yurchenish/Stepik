c4f5a8508faeded62bf98885ff572511a8c61df1
yaroslav.yurchenko <yurchenish@yandex.ru>
2021-10-21 12:33:50 +0300

    new features

--- test_product_page.py
+++ test_product_page.py
@@ -1,9 +1,4 @@
-from .pages.main_page import MainPage
-
-
-def go_to_login_page(browser):
-    login_link = browser.find_element_by_css_selector("#login_link")
-    login_link.click()
+from pages.main_page import MainPage
 
 
 def test_guest_can_go_to_login_page(browser):
