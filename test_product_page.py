4d9e54430d4cdca125bdfd9e227a2bc49c17d762
yaroslav.yurchenko <yurchenish@yandex.ru>
2021-10-26 00:11:26 +0300

    refactor files

--- test_product_page.py
+++ test_product_page.py
@@ -1,23 +1,4 @@
-from pages.main_page import MainPage
-from pages.login_page import LoginPage
 from pages.product_page import ProductPage
-import time
-
-
-def test_guest_can_go_to_login_page(browser):
-    link = "http://selenium1py.pythonanywhere.com"
-    page = MainPage(browser, link)
-    page.open()
-    page.go_to_login_page()
-    login_page = LoginPage(browser, browser.current_url)
-    login_page.should_be_login_page()
-
-
-def test_guest_should_see_login_link(browser):
-    link = "http://selenium1py.pythonanywhere.com/"
-    page = MainPage(browser, link)
-    page.open()
-    page.should_be_login_link()
 
 
 def test_guest_can_add_product_to_basket(browser):
@@ -26,3 +7,4 @@ def test_guest_can_add_product_to_basket(browser):
     page.open()
     page.add_in_basket()
     page.solve_quiz_and_get_code()
+
