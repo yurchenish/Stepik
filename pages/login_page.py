0b190b701701046028771ad85fa553d869dd0341
yaroslav.yurchenko <yurchenish@yandex.ru>
2021-10-25 23:31:09 +0300

    new files, new auto

--- pages/login_page.py
+++ pages/login_page.py
@@ -1,4 +1,6 @@
 from .base_page import BasePage
+from .locators import LoginPageLocators
+import time
 
 
 class LoginPage(BasePage):
@@ -8,13 +10,13 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        # реализуйте проверку на корректный url адрес
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_URL), "Login url is not presented"
+        login_link = self.browser.find_element(*LoginPageLocators.LOGIN_URL)
+        login_link.click()
+        assert "login" in self.browser.current_url, "There is not login in url"
 
     def should_be_login_form(self):
-        # реализуйте проверку, что есть форма логина
-        assert True
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM)
 
     def should_be_register_form(self):
-        # реализуйте проверку, что есть форма регистрации на странице
-        assert True
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM)
--- pages/main_page.py
+++ pages/main_page.py
@@ -1,12 +1,14 @@
 from pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from pages.locators import MainPageLocators
+from pages.login_page import LoginPage
 
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        login_link = self.browser.find_element(By.CSS_SELECTOR, "#login_link")
-        login_link.click()
+        link = self.browser.find_element(*MainPageLocators.LOGIN_LINK)
+        link.click()
+        # return LoginPage(browser=self.browser, url=self.browser.current_url)
 
     def should_be_login_link(self):
         # * - указывает на то, что мы передали именно пару, и этот кортеж нужно распаковать.
