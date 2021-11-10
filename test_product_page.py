eeae8a0ac4614da46cc2c9abf8f17be4702ccc21
yaroslav.yurchenko <yurchenish@yandex.ru>
2021-11-11 01:29:05 +0300

    create pytest.ini

--- test_product_page.py
+++ test_product_page.py
@@ -1,5 +1,6 @@
 from pages.product_page import ProductPage
 from pages.basket_page import BasketPage
+from pages.base_page import BasePage
 import pytest
 
 
@@ -73,5 +74,17 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     page2.is_basket_empty()
     page2.should_be_basket_empty_message()
 
-# @pytest.mark.login_guest
-# class TestLoginFromMainPage:
+
+@pytest.mark.login_guest
+class TestLoginFromMainPage:
+    def test_guest_can_go_to_login_page(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.go_to_login_page()
+
+    def test_guest_should_see_login_link(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/"
+        page = BasePage(browser, link)
+        page.open()
+        page.should_be_login_link()
