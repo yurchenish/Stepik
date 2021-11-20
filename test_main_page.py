c7ce747285313bf3af4e0e9cd1e3cae783eedab0
yaroslav.yurchenko <yurchenish@yandex.ru>
2021-11-20 15:50:14 +0300

    finish

--- test_main_page.py
+++ test_main_page.py
@@ -1,6 +1,5 @@
 from pages.main_page import MainPage
-from pages.login_page import LoginPagegit
-import time
+from pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
@@ -17,4 +16,3 @@ def test_guest_should_see_login_link(browser):
     page = MainPage(browser, link)
     page.open()
     page.should_be_login_link()
-
